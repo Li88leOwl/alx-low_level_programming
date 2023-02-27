7d6bd6b12d55af1f8fea998bd0c8fdbe6330c5e5
Li88leOwl <li88leowl@gmail.com>
2023-02-27 07:22:44 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code for ALX Program
- *
- * Return: Always 0.
+ * print_array - Prints an inputted number of elements
+ *               of an array of integers.
+ * @a: The array of integers.
+ * @n: The number of elements to be printed.
  */
-int main(void)
+void print_array(int *a, int n)
 {
-	int array[6];
+	int index;
 
-	array[0] = 98;
-	array[1] = 402;
-	array[2] = -198;
-	array[3] = 298;
-	array[4] = -1024;
-	array[5] = -200003;
-	print_array(array, 6);
-	return (0);
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
 }
