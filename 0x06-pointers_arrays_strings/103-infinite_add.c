2420598a5e017da2f0488fdd4218469c1dc2ccb4
Li88leOwl <li88leowl@gmail.com>
2023-03-01 22:56:23 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,85 +1,78 @@
 #include "main.h"
 
 /**
- * carry_from_prev - carry overflow from previous index to current index
- * @b: the buffer to operate on
- * @i: the index to carry to
- *
- * Return: void
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
-void carry_from_prev(char *b, int i)
+
+void rev_string(char *n)
 {
-	b[i] = b[i - 1] / 10;
-	b[i - 1] %= 10;
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
 }
 
-
 /**
- * find_str_end - find the end of a string
- * @s: the string to search
- *
- * Return: a pointer to the end of the string
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
-char *find_str_end(char *s)
-{
-	char *s_end;
 
-	for (s_end = s; *s_end; ++s_end)
-		;
-	return (s_end);
-}
-
-
-/**
- * infinite_add - adds two numbers
- * @n1: the first number to add
- * @n2: the other number to add
- * @r: the buffer to store the result
- * @size_r: the size of the buffer
- *
- * Return: a pointer to the buffer r, unless the result does not fit into r,
- * in which case return 0.
- */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	char *n1_end, *n2_end;
-	int i, j;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	for ( ; n1[1] && *n1 == '0'; ++n1)
-		;
-	for ( ; n2[1] && *n2 == '0'; ++n2)
-		;
-	n1_end = find_str_end(n1);
-	n2_end = find_str_end(n2);
-	for (i = 0; n1 < n1_end || n2 < n2_end; ++i)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+	j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		if (i + 1 >= size_r)
-			return (0);
-		if (i && r[i - 1] > 9)
-			carry_from_prev(r, i);
+		if (i < 0)
+			val1 = 0;
 		else
-			r[i] = 0;
-		if (n1 < n1_end)
-			r[i] += *(--n1_end) - '0';
-		if (n2 < n2_end)
-			r[i] += *(--n2_end) - '0';
-	}
-	if (r[i - 1] > 9)
-	{
-		if (i + 1 >= size_r)
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
 			return (0);
-		carry_from_prev(r, i++);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	r[i--] = '\0';
-	for (j = 0; j < i; --i, ++j)
-	{
-		r[i] ^= r[j];
-		r[j] ^= r[i];
-		r[i] ^= r[j];
-		r[i] += '0';
-		r[j] += '0';
-	}
-	if (j == i)
-		r[i] += '0';
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
